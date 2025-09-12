ab9e9527344ecc4f418f40484557799407b7f8ee
Surfsol <surfsol@protonmail.com>
2025-09-12 09:03:20 -0300

    update Session

--- feedback-agent/src/Session.tsx
+++ feedback-agent/src/Session.tsx
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
+import rawFeedback from "./encounters.json";
+
+const feedback = rawFeedback as AllTasks;
 
 interface SessionProps {
   encounter_num: string;
 }
 
+interface TaskObj {
+  task: string;
+  success: boolean; // or boolean if you want
+  notes: string;
+}
+
+interface TaskNumProps {
+  [key: string]: TaskObj;
+}
+
+interface AllTasks {
+  [key: string]: TaskNumProps;
+}
+
 const Session: React.FC<SessionProps> = ({ encounter_num }) => {
   const [names, setNames] = useState<string[]>([]);
-  const [newName, setNewName] = useState<string>("");
-  const [tasks, setTasks] = useState<[]>([]);
+  const [tasks, setTasks] = useState<TaskNumProps>(feedback[encounter_num]);
+  const [tasksArray] = useState<string[]>(Object.keys(feedback[encounter_num]));
+  const [taskNum, settaskNum] = useState<number>(0);
+  console.log({ feedback });
 
-  console.log("in sessions", encounter_num);
+  console.log("in sessions", { tasksArray });
   return (
     <>
       <div>This Session</div>
@@ -18,32 +37,36 @@ const Session: React.FC<SessionProps> = ({ encounter_num }) => {
         {" "}
         Name :{" "}
         <input
-          id='add-name'
+          id='add-name-1'
           type='text'
-          value={names ? names[names.length] : ""}
-          onChange={(e) => setNewName(e.target.value)}
+          value={names[0]}
+          onChange={(e) =>
+            setNames((prev) => [...prev, (names[0] = e.target.value)])
+          }
           style={{ width: "60px" }} // small input box
         />
-        <button onClick={() => setNames((prev) => [...prev, newName])}></button>
-      </div>
-      {names.length > 0 && tasks.length > 0 &&
-        names.map((name) => {
+        {tasksArray &&
+          tasksArray.map((item) => {
             return (
-           {tasks.length > 0 && tasks.map((task) => (
-          
-            <div>
-              <div>{name} {task}</div>
-              <input
-                id={`${name}-${task}`}
-                type='text'
-                value={names ? names[names.length] : ""}
-                onChange={(e) => setNewName(e.target.value)}
-                style={{ width: "60px" }} // small input box
-              />
-            </div>
-          ))})
-        })}
-      <div></div>
+              <div key={`name-${item}`}>
+                {" "}
+                {item}
+                <input
+                  id={`feed-${item}`}
+                  type='text'
+                  value={tasks[item]["notes"]}
+                  onChange={(e) =>
+                    setTasks({
+                      ...tasks,
+                      [item]: { ...tasks["item"], ["notes"]: e.target.value },
+                    })
+                  }
+                  style={{ width: "60px" }} // small input box
+                />
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
