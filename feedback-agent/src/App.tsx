03dbe3178ae0a027453330dc8468da47247780bd
Surfsol <surfsol@protonmail.com>
2025-09-13 10:35:40 -0300

    update add and sessions

--- feedback-agent/src/App.tsx
+++ feedback-agent/src/App.tsx
@@ -1,16 +1,47 @@
 import { useState } from "react";
 import Session from "./Session";
+import rawFeedback from "./encounters.json";
+
+const feedback = rawFeedback as AllTasks;
+
+interface AllTasks {
+  [key: string] : TaskNumProps
+}
+
+interface TaskObj {
+  task: string;
+  success: boolean; // or boolean if you want
+  notes: string;
+}
+
+interface TaskNumProps {
+  [key: string]: TaskObj;
+}
+
+// students keyed by name
+type StudentTasks = Record<string, TaskNumProps>;
+
+interface SessionStudentTasks {
+    [sessionCode: string] : StudentTasks
+}   
 
 function App() {
-  const [sessions, setSessions] = useState<string[]>([]);
+  const [sessions, setSessions] = useState<SessionStudentTasks>({});
   const [createNew, setCreateNew] = useState<string>('');
+  const [newName, setNewName] = useState('')
 //  const [encounter, setEncounter] = useState<number | undefined>();
 
   const handleNew = () => {
+    const now = new Date().toString();
+    const match = now.match(/\b\d{2}:\d{2}:\d{2}\b/);
     // add a new session for encounter number
     if (createNew) {
-      setSessions((prev) => [...prev, createNew]);
+      const taskObj = feedback[createNew]
+      const studentTasks: TaskNumProps = JSON.parse(JSON.stringify(taskObj));
+      const objKey = `${createNew}-${match}`
+      setSessions((prev) => ({...prev, [objKey] : {...prev[objKey], [newName] : studentTasks}}));
       setCreateNew('');
+      setNewName('')
     }
   };
   console.log({sessions})
@@ -29,14 +60,22 @@ function App() {
           style={{ width: "60px" }} // small input box
         />
         <br />
+        <input
+          id='createNew-name'
+          type='text'
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+          style={{ width: "60px" }} // small input box
+        />
+        <br />
       </div>
       <button onClick={handleNew}>Create New Session</button>
       <div>Sessions</div>
-      {sessions.length > 0 &&
-        sessions.map((item) => (
-           <div key={item} id={`session-${item}`}>
+      {Object.keys(sessions).length > 0 &&
+        Object.keys(sessions).map((code) => (
+           <div key={code} id={`session-${code}`}>
             {" "}
-            <Session encounter_num={item} />{" "}
+            <Session session_code={code} current_session = {sessions[code]} setSessions={setSessions} />{" "}
           </div>
         ))}
     </>
--- feedback-agent/src/Session.tsx
+++ feedback-agent/src/Session.tsx
@@ -3,8 +3,8 @@ import rawFeedback from "./encounters.json";
 
 const feedback = rawFeedback as AllTasks;
 
-interface SessionProps {
-  encounter_num: string;
+interface AllTasks {
+  [key: string]: TaskNumProps;
 }
 
 interface TaskObj {
@@ -17,27 +17,42 @@ interface TaskNumProps {
   [key: string]: TaskObj;
 }
 
-interface AllTasks {
-  [key: string]: TaskNumProps;
-}
-
 // students keyed by name
 type StudentTasks = Record<string, TaskNumProps>;
 
-const Session: React.FC<SessionProps> = ({ encounter_num }) => {
-  const [names, setNames] = useState<StudentTasks>({});
-  const [newName, setNewName] = useState<string>("");
-  const [tasks] = useState<TaskNumProps>(feedback[encounter_num]);
+interface SessionStudentTasks {
+  [sessionCode: string]: StudentTasks;
+}
 
-  console.log("in sessions", { names });
+interface SessionProps {
+  session_code: string;
+  current_session: StudentTasks;
+  setSessions: React.Dispatch<React.SetStateAction<SessionStudentTasks>>;
+}
+
+const Session: React.FC<SessionProps> = ({
+  session_code,
+  current_session,
+  setSessions,
+}) => {
+  const [encounter_num] = useState(() => {
+    const match = session_code.match(/^([^-\s]+)/);
+    return match ? match[1] : "";
+  });
+  const [newName, setNewName] = useState<string>("");
+
+  console.log({ session_code, current_session });
 
   const AddStudent = () => {
-    if (newName == "") {
-      // alert add new name
+    if (newName != "" && encounter_num) {
+      const tasks = feedback[encounter_num];
+      const studentTasks: TaskNumProps = JSON.parse(JSON.stringify(tasks));
+      setSessions((prev) => ({
+        ...prev,
+        [session_code]: { ...prev[session_code], [newName]: studentTasks },
+      }));
+      setNewName("");
     }
-    const studentTasks: TaskNumProps = JSON.parse(JSON.stringify(tasks));
-    setNames((prev) => ({ ...prev, [newName]: studentTasks }));
-    setNewName("");
   };
 
   const Update_Success_Notes = (
@@ -46,15 +61,24 @@ const Session: React.FC<SessionProps> = ({ encounter_num }) => {
     task_num: string,
     type: string
   ) => {
-    console.log({ value, person, task_num, type });
-    //setNames(prev => ...prev)
-    setNames((prev) => ({
+
+    setSessions((prev) => ({
       ...prev,
-      [person]: {
-        ...prev[person],
-        [task_num]: { ...prev[person][task_num], [type]: value },
+      [session_code]: {
+        ...prev[session_code],
+        [person]: {
+          ...prev[session_code][person],
+          [task_num]: {
+            ...prev[session_code][person][task_num],
+            [type]: value,
+          },
+        },
       },
     }));
+  };
+
+  const handleGetFeedBack = (num: string) => {
+    console.log(num);
   };
 
   return (
@@ -72,18 +96,21 @@ const Session: React.FC<SessionProps> = ({ encounter_num }) => {
           style={{ width: "60px" }} // small input box
         />
         <button onClick={AddStudent}>Add</button>
-        {Object.keys(names).length > 0 &&
-          Object.keys(names).map((person) => (
+        <button onClick={() => handleGetFeedBack(encounter_num)}>
+          Get FeedBack
+        </button>
+        {Object.keys(current_session).length > 0 &&
+          Object.keys(current_session).map((person) => (
             <div key={`student-${person}`}>
               <h3>{person}</h3>
-              {Object.keys(names[person]).map((task_num) => {
+              {Object.keys(current_session[person]).map((task_num) => {
                 return (
                   <div key={`name-${person}-${task_num}`}>
                     {task_num}
                     <input
                       id={`${person}-${task_num}-success`}
                       type='checkbox'
-                      checked={names[person][task_num]["success"]}
+                      checked={current_session[person][task_num]["success"]}
                       onChange={(e) =>
                         Update_Success_Notes(
                           e.target.checked,
@@ -96,7 +123,7 @@ const Session: React.FC<SessionProps> = ({ encounter_num }) => {
                     <input
                       id={`notes-${person}-${task_num}`}
                       type='text'
-                      value={names[person][task_num]["notes"]}
+                      value={current_session[person][task_num]["notes"]}
                       onChange={(e) =>
                         Update_Success_Notes(
                           e.target.value,
